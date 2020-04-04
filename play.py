da3ebbe46d2e7be9cac540c8804346c7ec2f945b
fbunaren <fransiscus.emmanuel@ui.ac.id>
2020-04-04 20:41:44 +0700

    Beautifying codes to follow Python PEP Guidelines

--- play.py
+++ play.py
@@ -3,33 +3,35 @@
 
 # This file is for playing against AI
 
-#Libraries
 from engine import Engine
 from board import Board
 import os
+
 
 def main():
     board = Board()
 
     print("Bunaren TicTacToe AI")
     print("========================")
-    symbol= input("Choose your symbol (X/O) : ").upper()
+    symbol = input("Choose your symbol (X/O) : ").upper()
 
     engine = Engine()
     engine.setSymbol("O" if symbol == "X" else "X")
 
-    #Computer Starts first if X is chosen by player
+    # Computer Starts first if X is chosen by player
     if symbol == "O":
         board.move(engine.bestMove(board.board))
     board.print()
 
     while not board.isOver():
-        pos = list(map(int,input("Draw " + symbol + " at [row column] : ").split()))
+        pos = input("Draw " + symbol + " at [row column] : ").split()
+        pos = list(map(int, pos))
         pos = [i-1 for i in pos]
         pos = pos[0] * 3 + pos[1]
         while not board.isValid(pos):
             print("Invalid Position")
-            pos = list(map(int,input("Draw " + symbol + " at [row column] : ").split()))
+            pos = input("Draw " + symbol + " at [row column] : ").split()
+            pos = list(map(int, pos))
             pos = [i-1 for i in pos]
             pos = pos[0] * 3 + pos[1]
         board.move(pos)
@@ -37,7 +39,7 @@ def main():
         if board.isOver():
             break
 
-        #Computer's Turn
+        # Computer's Turn
         board.move(engine.bestMove(board.board))
         board.print()
 
@@ -52,7 +54,7 @@ def main():
     else:
         print("You lose!")
 
-    #Play Again?
+    # Play Again?
     play_again = input("\nPlay again? [y/n] : ").lower()
     if play_again == "y":
         os.system("cls")
